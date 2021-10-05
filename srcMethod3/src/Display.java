9d1fea9d3be71805a7cdec9f49722dfa19de5038
Joshua <74574379+JoshuaWhite0110@users.noreply.github.com>
2021-10-05 13:52:12 -0400

    Update Display.java

--- srcMethod3/src/Display.java
+++ srcMethod3/src/Display.java
@@ -5,8 +5,12 @@ public class Display {
 
     }
 
-    public void displayText(String text) {//subscriber method, waits for nottification of product input and reacts to it 
+    public void displayText(String text) { 
         System.out.println("Displaying the Product: ");
         System.out.println(text);
+    }
+    public void notifyText(String text){//subscriber method, waits for nottification of product input and reacts to it
+        System.out.print("Subscription Recived by Display, ");
+        System.out.println(text + " was order");
     }
 }
