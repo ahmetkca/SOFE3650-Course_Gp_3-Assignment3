fe841e6f9de4f971539d82c041b3dd6c9f9a0904
Joshua <74574379+JoshuaWhite0110@users.noreply.github.com>
2021-10-05 22:31:54 -0400

    Update TicketPrinter.java

--- srcMethod3/src/TicketPrinter.java
+++ srcMethod3/src/TicketPrinter.java
@@ -1,11 +1,26 @@
 package src;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class TicketPrinter {
-   
-    public TicketPrinter() {
-         
+public class TicketPrinter extends Observer{
+    List<Observer> obs = new ArrayList<Observer>();
+    
+    public TicketPrinter() {  
     }
+
+    public void notiy(){
+
+    }
+
+    @Override
+    public void onNotify(double diss, String text){
+        this.discount= diss;
+        System.out.println("discount of "+ this.discount +"% was applied");
+        System.out.println("Printing the Product");
+        System.out.println(text);
+    }
+
 
     public void displayText(String text) { 
         System.out.println("Printing the Product");
